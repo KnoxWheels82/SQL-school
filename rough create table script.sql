5c964fcced8301d97b1c9418a7e50a319f3877f0
Jonathan Smith <KnoxParalegal2015@outlook.com>
2025-06-23 16:10:32 -0400

    Delete rough create table script.sql

--- rough create table script.sql
+++
@@ -1,8 +0,0 @@
-/*=================================*/
-/*	Table: Table name		       */
-/*=================================*/
-CREATE TABLE TableName (
-	columnName PK		int,
-	columnName			nvarchar (max number of numbers)	not null,
-	CONSTRAINT PK_CUSTOMERS PRIMARY KEY (FirstColumn)
-	)
